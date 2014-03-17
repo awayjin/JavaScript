def5d462577936677ab63369a44b114d9d8cfb66
awayCompany <258246377@qq.com>
2014-03-17 17:52:52 +0800

    jan dialog title v1.0

---
+++ Demo/06AJAX.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: jinwei
+ * Date: 14-3-5
+ * Time: 上午11:21
+ * To change this template use File | Settings | File Templates.
+ */
+
+echo "jinwei text";
---
+++ jQuery/demo/test1.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: jinwei
+ * Date: 14-2-21
+ * Time: 下午2:33
+ * To change this template use File | Settings | File Templates.
+ */
+
+echo "jinwei";
