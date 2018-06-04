1a8f7a70440ae8071bd9a29bcd25f2df082862da
awayjin <258246377Z@qq.com>
2018-06-04 23:12:26 +0800

    d

--- vue/my-mvvm/src/mvvm.js
+++ vue/my-mvvm/src/mvvm.js
@@ -5,10 +5,35 @@
 function MVVM (options) {
   this.$options = options || {}
 
-  this.$data = options.data
-  this.$methods = options.methods
+  // this.$data = options.data
+  // this.$methods = options.methods
 
   var data = this._data = this.$options.data
-  observe(data, this)
+  var me = this
 
+  // 数据代理
+  // 实现 vm.xxx -> vm._data.xxx
+  Object.keys(data).forEach(function (key) {
+    me._proxyData(key)
+  })
+
+  // observe(data, this)
+
+}
+
+MVVM.prototype = {
+  _proxyData: function (key, setter, getter) {
+    var me = this
+    setter = setter ||
+        Object.defineProperty(me, key, {
+          enumerable: true,
+          configurable: false,
+          get: function proxyGetter () {
+            return me._data[key]
+          },
+          set: function proxySetter (newVal) {
+            me._data[key] = newVal
+          }
+        })
+  }
 }
