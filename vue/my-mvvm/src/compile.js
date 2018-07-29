72ea32efd21a0d6fd28c477d1010d3b757fb9cea
awayjin <258246377Z@qq.com>
2018-07-29 23:44:19 +0800

    unsettled

--- vue/my-mvvm/src/compile.js
+++ vue/my-mvvm/src/compile.js
@@ -238,9 +238,10 @@ var updater = {
   },
   // 文本节点指令替换
   textUpdater: function (node, value, oldValue) {
-    var reg =  oldValue || /(\{\{(.*)\}\})/gi
+    // var reg =  oldValue || /(\{\{(.*)\}\})/gi
+    var reg =  oldValue || /(\{\{(\w+)\}\})/g
     var text = node.textContent.replace(reg, value)
-    // console.log(oldValue)
+    console.log(text)
     node.textContent = typeof text === 'undefined' ? '' : text
     // node.textContent = typeof value === 'undefined' ? '' : value
   }
