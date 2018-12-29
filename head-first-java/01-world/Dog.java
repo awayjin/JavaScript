b8bd9134b3b892e8cd2921387e2fbad55d3e958e
awayjin <258246377@qq.com>
2018-12-29 17:14:22 +0800

    Ningja

---
+++ head-first-java/01-world/Dog.java
@@ -0,0 +1,16 @@
+class Dog {
+    int size;
+    String bread;
+    String name;
+
+    void bark () {
+        System.out.println('Ruff.!')
+    }
+}
+
+class DogTestDriver {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.size = 40;
+    }
+}
