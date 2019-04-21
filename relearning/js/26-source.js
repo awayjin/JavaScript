54dab6acadc5b161a48b190e4dc212db9707c592
金伟 <jinwei@jinweideMacBook-Pro.local>
2019-04-22 07:59:58 +0800

    module

---
+++ relearning/js/26-source.js
@@ -0,0 +1,12 @@
+export const console = () => {
+  window.console.log('source, js')
+}
+
+export const consoleTwo = () => {
+  window.console.log('consoleTwo module')
+}
+
+export default {
+  console,
+  consoleTwo
+}
