cf2303d6450b2b4ae38a7f70172719c76c6d78a1
金伟 <jinwei@jinweideMacBook-Pro.local>
2019-06-13 07:56:51 +0800

    publish-subscribe

--- vue/my-mvvm/src/mvvm.js
+++ vue/my-mvvm/src/mvvm.js
@@ -2,6 +2,23 @@
  * Created by jinwei on 2018/6/3.
  */
 
+/**
+ *
+ *
+ > Observer 数据监听器，能够对数据对象的所有属性进行监听，如有变动可拿到最新值并通知订阅者，
+ 内部采用Object.defineProperty的getter和setter来实现。
+
+ > Compile 指令解析器，它的作用对每个元素节点的指令进行扫描和解析，
+ 根据指令模板替换数据，以及绑定相应的更新函数。
+
+ > Watcher 订阅者， 作为连接 Observer 和 Compile 的桥梁，
+ 能够订阅并收到每个属性变动的通知，执行指令绑定的相应回调函数。
+
+ > Dep 消息订阅器，内部维护了一个数组，用来收集订阅者（Watcher），
+ 数据变动触发 notify 函数，再调用订阅者的 update 方法。
+ *
+ */
+
 function MVVM (options) {
   this.$options = options || {}
 
--- vue/my-mvvm/src/watcher.js
+++ vue/my-mvvm/src/watcher.js
@@ -14,6 +14,26 @@
  * 3. hasOwnProperty, 方法会返回一个布尔值，指示对象自身属性中是否具有指定的属性
  * var obj = { a: 22}; obj.hasOwnProperty('a')
  */
+
+
+
+/**
+ *
+ *
+ > Observer 数据监听器，能够对数据对象的所有属性进行监听，如有变动可拿到最新值并通知订阅者，
+ 内部采用Object.defineProperty的getter和setter来实现。
+
+ > Compile 指令解析器，它的作用对每个元素节点的指令进行扫描和解析，
+ 根据指令模板替换数据，以及绑定相应的更新函数。
+
+ > Watcher 订阅者， 作为连接 Observer 和 Compile 的桥梁，
+ 能够订阅并收到每个属性变动的通知，执行指令绑定的相应回调函数。
+
+ > Dep 消息订阅器，内部维护了一个数组，用来收集订阅者（Watcher），
+ 数据变动触发 notify 函数，再调用订阅者的 update 方法。
+ *
+ */
+
 function Watcher(vm, expOrFn, cb) {
   this.vm = vm
   this.expOrFn = expOrFn
