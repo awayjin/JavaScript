8f21e4e01e1688d5c8ca49d6bab3604cff11f162
金伟 <jinwei@jinweideMacBook-Pro.local>
2019-08-06 22:58:10 +0800

    ts-function

--- typescript/ts-in-action/src/part1.base/09.function.ts
+++ typescript/ts-in-action/src/part1.base/09.function.ts
@@ -25,3 +25,37 @@ interface add4Func {
   readonly id: number;
   (x: number, y: number): number
 }
+
+// 参数可选
+function add5(x: number, y?: number) {
+  return y? x + y: x;
+}
+add5(5)
+
+// 默认参数
+function add6(x: number, y = 1) {
+  return x + y
+}
+console.log('默认参数 add6:', add6(4))
+
+// 剩余参数
+function add7(x: number, ...rest: number[]) {
+  return x + rest.reduce((acc, cur) => acc + cur)
+}
+console.log('剩余参数 add7:', add7(4, 1, 2, 3, 5))
+
+// 函数重载
+function add8(...rest: number[]): number;
+function add8(...rest: string[]): string;
+function add8(...rest: any[]): any {
+  let first = rest[0]
+  if (typeof first === 'string') {
+    return rest.join('-')
+  }
+
+  if (typeof first === 'number') {
+    return rest.reduce((acc, cur) => acc + cur)
+  }
+}
+console.log('函数重载 add8 number:', add8( 1, 2, 3, 5))
+console.log('函数重载 add8 string:', add8('a', 'b'))
