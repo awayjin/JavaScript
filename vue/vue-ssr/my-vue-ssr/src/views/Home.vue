c8a6abd20a13c466235094aa189dae64c9e455c0
awayjin <258246377@qq.com>
2019-08-09 16:38:54 +0800

    lintfix

--- vue/vue-ssr/my-vue-ssr/src/views/Home.vue
+++ vue/vue-ssr/my-vue-ssr/src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    This is a home's page.
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
--- vue/vue-ssr/my-vue-ssr/vue.config.js
+++ vue/vue-ssr/my-vue-ssr/vue.config.js
@@ -1,10 +1,9 @@
 const isNode = process.env.WEBPACK_TARGET === 'node'
-const TARGET = isNode ? 'server' : 'client'
 // node server 端打包
 const serverConfig = require('./server/webpack.server.config.js')
 // 客户端打包
 const clientConfig = require('./server/webpack.client.config.js')
-const merge = require('lodash.merge')
+// const merge = require('lodash.merge')
 
 console.log('process.env.WEBPACK_TARGET:', process.env.WEBPACK_TARGET)
 console.log('process.env.NODE_ENV:', process.env.NODE_ENV)
@@ -14,23 +13,23 @@ module.exports = {
     port: 5001
   },
   css: {
-    // extract: process.env.NODE_ENV === 'production'
-    extract: false
+    extract: process.env.NODE_ENV === 'production'
+    // extract: false
   },
-  configureWebpack: () => ({
+  configureWebpack: {
     // 客户端渲染入口
     // entry: `./src/entry-${TARGET}.js`
     ...(isNode ? serverConfig : clientConfig)
     // ...webpackConfig
-  }),
-  chainWebpack: config => {
-    config.module
-      .rule('vue')
-      .use('vue-loader')
-      .tap(options =>
-        merge(options, {
-          optimizeSSR: false
-        })
-      )
-  }
+  },
+  // chainWebpack: config => {
+  //   config.module
+  //     .rule('vue')
+  //     .use('vue-loader')
+  //     .tap(options =>
+  //       merge(options, {
+  //         optimizeSSR: false
+  //       })
+  //     )
+  // }
 }
