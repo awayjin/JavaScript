8446bb4649fadddd312001403ae64ecb15b3294a
金伟 <jinwei@jinweideMacBook-Pro.local>
2019-09-02 08:05:33 +0800

    feat: review algorithm

---
+++ data-structure-algorithms/code-three/02.ecma-ts/02-01.node.mjs
@@ -0,0 +1,5 @@
+const obj =  {
+  a: 2
+}
+
+export default obj
---
+++ data-structure-algorithms/code-three/02.ecma-ts/demo.js
@@ -0,0 +1,5 @@
+// import * as a from './02-01.node.mjs'
+import a from './02-01.node.mjs'
+// import { a } from './02-01.node.mjs' // error
+
+console.log(a)
