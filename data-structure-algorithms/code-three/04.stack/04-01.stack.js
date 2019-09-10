6926789ef90dba414d9ed8ace6a61ccbcf0e561f
金伟 <jinwei@jinweideMacBook-Pro.local>
2019-09-10 08:09:34 +0800

    feat(stack): to decimalToBinary

--- data-structure-algorithms/code-three/04.stack/04-01.stack.js
+++ data-structure-algorithms/code-three/04.stack/04-01.stack.js
@@ -5,9 +5,16 @@ class Stack {
 
   push (element) {
     this.items.push(element)
+    return this
   }
+  // pop
   pop () {
-    this.items.pop()
+    return this.items.pop()
+  }
+  // toString
+  toString () {
+    // console.log(this.items)
+    return this.items.toString()
   }
   peek () {
     return this.items[this.items.length - 1]
@@ -24,6 +31,30 @@ class Stack {
   print(){
     console.log(this.toString())
   }
+}
+
+// 十进制转二进制
+export function decimalToBinary (decNumber) {
+  const stack = new Stack()
+  let number = decNumber
+  let rem
+  let binaryString = ''
+
+  // 除法结果不为 0 时，获得余数
+  while (number > 0) {
+    // rem = Math.floor(number % 2) // remainder
+    rem = number % 2 // remainder
+    // 余数放栈里
+    stack.push(rem)
+    number = Math.floor(number / 2)
+  }
+
+  console.log('stack:', stack)
+  while (!stack.isEmpty()) {
+    binaryString += stack.pop().toString()
+  }
+
+  return binaryString
 }
 
 export default Stack
