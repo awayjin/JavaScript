d81f043700fefafb970e227b0ffe93cd1bafcdda
金伟 <jinwei@jinweideMacBook-Pro.local>
2019-09-28 07:47:03 +0800

    feat: process

--- nodejs/action.geektime/02.tech-pre/game.js
+++ nodejs/action.geektime/02.tech-pre/game.js
@@ -1,20 +1,19 @@
-console.log(Date)
-console.log(Math)
-console.log(setTimeout)
-console.log(__dirname)
-console.log(__filename)
-console.log('process.argv:', process.argv)
+
 
 // 石头剪刀布游戏
 // process.argv 属性返回一个数组，其中包含当启动 Node.js 进程时传入的命令行参数
 
-let playAction = process.argv[process.argv.length - 1]
-console.log('playAction:', playAction)
-let playArr = ['rock', 'scissors', 'paper']
 
-if (!playArr.includes(playAction)) {
-  console.log('请输入rock或paper或scissor')
-} else {
+module.exports = (playAction) => {
+  let playArr = ['rock', 'scissors', 'paper']
+
+  if (playArr.includes(playAction) === false) {
+    console.log('请输入rock或paper或scissors, 你输入了:', playAction)
+    // throw new Error('invalid playerActions')
+    return
+  }
+
+  console.log('我出了 playAction:', playAction)
   let random = Math.random() * 3
   let computerAction
   if (random > 2) {
@@ -30,15 +29,18 @@ if (!playArr.includes(playAction)) {
 
   if (playAction === computerAction) {
     console.log('平局')
+    return 0
   } else if (
     (computerAction === 'rock' && playAction === 'scissors') ||
     (computerAction === 'paper' && playAction === 'rock') ||
     (computerAction === 'scissors' && playAction === 'paper')
   ) {
     console.log('你输了')
+    return -1
   } else {
     console.log('你赢了')
+    return 1
   }
 
-}
 
+}
---
+++ nodejs/action.geektime/02.tech-pre/index.js
@@ -0,0 +1,22 @@
+const game = require('./game')
+
+// let playAction = process.argv[process.argv.length - 1]
+
+// const result = game(playAction)
+// console.log('result:', result)
+
+let count = 0
+process.stdin.on('data', e => {
+  const playAction = e.toString().trim()
+  const result = game(playAction)
+
+  if (result === 1) {
+    count++
+    console.log(`你赢了 ${count} 次`)
+  }
+
+  if (count === 3) {
+    console.log(`你赢了 ${count} 次, 我不玩了，退出！`)
+    process.exit()
+  }
+})
