8863da46b430ba9f2b2cece50b2bca5d96c4b304
金伟 <jinwei@jinweideMacBook-Pro.local>
2019-10-08 07:57:21 +0800

    feat: interface

---
+++ typescript/ts-in-action/src/part1.base/01.interface.ts
@@ -0,0 +1,77 @@
+interface Person01 {
+  // 有时候我们希望对象中的一些字段只能在创建的时候被赋值，
+  // 那么可以用 readonly 定义只读属性
+  readonly id: number;
+  name: string;
+  // 可选属性
+  age?: number;
+  // 任意属性
+  // 字符串索引签名, 用任意字符串来索引 List， 这样 List 就支持多个属性了
+  [x: string]: any;
+}
+
+// 一旦定义了任意属性，那么确定属性和可选属性的类型都必须是它的类型的子集
+let tom: Person01 = {
+  // 只读的约束存在于第一次给对象赋值的时候，而不是第一次给只读属性赋值的时候
+  id: 87,
+  name: 'Tom',
+  age: 24,
+  gender: 'male'
+}
+// tom.id = 44
+
+interface Alarm {
+  alert (): void;
+}
+
+// 接口的另一个用途，对类的一部分行为进行抽象
+let Jim: Person01 = {
+  id: 11,
+  name: 'Jim'
+}
+
+class Door {}
+
+class SecurityDoor extends Door implements Alarm {
+  alert () {
+    console.log('SecurityDoor alert.')
+  }
+}
+
+class Car implements Alarm {
+  alert(): void {
+    console.log('Car alert')
+  }
+}
+
+interface Light {
+  lightOn (): void;
+  lightOff (): void;
+}
+
+// 一个类可以实现多个接口
+class Car01 implements Alarm, Light{
+  alert () {}
+  lightOff () {}
+  lightOn () {}
+}
+
+// 接口继承接口
+// 接口与接口之间可以是继承关系：
+interface LightableAlarm extends Alarm {
+  lightOn (): void;
+  lightOff (): void;
+}
+
+// 接口也可以继承类
+class Point01 {
+  x: number = 1;
+  y: number = 2;
+}
+interface Point3d extends Point01{
+  z: number;
+}
+
+let point3d: Point3d = { x: 11, y: 22, z: 3}
+
+// 混合类型
--- typescript/ts-in-action/src/part1.base/12.class-interface.ts
+++ typescript/ts-in-action/src/part1.base/12.class-interface.ts
@@ -1,4 +1,51 @@
+/**
+ * 类类型接口
+ * 类实现接口 - implements
+ *  接口只能约束类的共有成员。
+ *  接口可以像类一样相互继承。
+ *
+ * 接口继承接口
+ * interface Man extends Human
+ *
+ * 接口继承类
+ * // 同时继承
+ interface Boy extends Man, Child {}
 
+ 6259760213119294
+
+ */
+
+interface Animal12 {
+  // new (name: string): void;
+  age: number;
+  eat: string;
+  run (): string;
+}
+
+interface AnimalList {
+  data: Animal12[]
+}
+
+let duck12: Animal12 = {
+  age: 12,
+  eat: 'rice',
+  run () {
+    return this.eat
+  }
+}
+
+// 类实现接口 implements
+class Dog12 implements Animal12 {
+  options: any;
+  constructor(options: object) {
+    this.options = options
+  }
+  age: number = 3;
+  eat: string = 'grass';
+  run () {
+    return this.eat;
+  }
+}
 
 interface Human {
   name: string;
@@ -15,15 +62,16 @@ class Asian implements Human {
   sleep (){}
 }
 
-// 接口继承
+// 接口继承接口
 interface Man extends Human {
-  run(): void
+  run(): void;
 }
 
 interface Child {
   cry(): void
 }
 
+// 同时继承
 interface Boy extends Man, Child {}
 
 let boy: Boy = {
@@ -35,14 +83,22 @@ let boy: Boy = {
 
 class Auto {
   state = 1
+  life: number = 3;
   // private state2 = 1
 }
+
+// 接口继承类 - 相当于把类的成员都抽象出来
 interface AutoInterface extends Auto {
 
 }
-// class Cd implements AutoInterface {
-//   state1 = 1
-// }
+
+// 实现
+class CD implements AutoInterface {
+  state = 1;
+  life =  33
+}
+
+//
 class Bus extends Auto implements AutoInterface {
 
 }
