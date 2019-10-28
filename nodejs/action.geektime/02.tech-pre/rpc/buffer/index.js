fa460dea1ba03aba1745a93d03902a266a718e43
awayjin <258246377@qq.com>
2019-10-28 17:26:17 +0800

    feat: bit

--- nodejs/action.geektime/02.tech-pre/rpc/buffer/index.js
+++ nodejs/action.geektime/02.tech-pre/rpc/buffer/index.js
@@ -1,8 +1,17 @@
+// JS字符串与二进制的相互转化 https://www.cnblogs.com/it-deepinmind/p/7430025.html
+// 字符编码笔记：ASCII，Unicode 和 UTF-8:  http://www.ruanyifeng.com/blog/2007/10/ascii_unicode_and_utf-8.html
+
+// 1. bit就是位，也叫比特位，是计算机表示数据最小的单位
+// 2 .byte就是字节
+// 3.1byte=8bit
+// 4.1byte就是1B
+// 5.一个字符=2字节
+// 6.1KB=1024B
 
 // 创建一个 8 位长度的字符，每 1 位代表它的一个字符
 // 每一位都是 16 进制的数字
 // BE-LE 大端小端排布。高位跟低位的排布
-const buffer1 = Buffer.from('0123-4569') // 字符串
+const buffer1 = Buffer.from('0') // 字符串
 
 const buffer2 = Buffer.from([1, 2, 3, 4, 11, 55]) // 数字数组
 
@@ -12,8 +21,12 @@ console.log('buffer1:', buffer1)
 console.log('buffer2:', buffer2)
 console.log('buffer3:', buffer3)
 
-buffer2.writeInt8(12, 1) // 第二位写入12, 占用一位
+buffer1.writeInt8(12)
+console.log('writeInt8 buffer1:', buffer1)
+
+buffer2.writeInt8(12, 1) // 第二位写入12, 占用一位o[
 console.log('writeInt8 buffer2:', buffer2)
+// console.log('writeInt8 buffer2:', buffer2.readInt16BE())
 
 buffer2.writeInt16BE(512, 2) // 占用二位
 console.log('writeInt16BE buffer2:', buffer2)
