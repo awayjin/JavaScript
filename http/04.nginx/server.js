a6047b423793d4002ae9b9b2f3b7d3a6bddae209
金伟 <jinwei@jinweideMacBook-Pro.local>
2019-11-03 23:43:34 +0800

    feat: http nginx proxy

--- http/04.nginx/server.js
+++ http/04.nginx/server.js
@@ -2,7 +2,8 @@ const http = require('http')
 
 http.createServer((req, res, next) => {
   console.log(req.headers.host)
-  console.log(req.headers)
+  console.log(req.headers.referer)
+  console.log(req.url)
   res.writeHead(200)
   res.end('demo')
 }).listen(8888)
