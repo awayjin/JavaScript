c6d0f09da8fc67ba61c52e4788711a2b8b559d4f
awayjin <258246377@qq.com>
2019-11-12 19:09:58 +0800

    feat: buffer

--- data-structure-algorithms/source/code-three/13.sort-search/sort.js
+++ data-structure-algorithms/source/code-three/13.sort-search/sort.js
@@ -95,9 +95,55 @@ function merge(left, right) {
   )
 }
 
+// 5. 快速排序
+// (1) 首先，从数组中选择一个值作为主元（pivot），也就是数组中间的那个值。
+// (2) 创建两个指针（引用），左边一个指向数组第一个值，右边一个指向数组最后一个值。移
+// 动左指针直到我们找到一个比主元大的值，接着，移动右指针直到找到一个比主元小的值，然后
+// 交换它们，重复这个过程，直到左指针超过了右指针。这个过程将使得比主元小的值都排在主元
+// 之前，而比主元大的值都排在主元之后。这一步叫作划分（partition）操作。
+// (3) 接着，算法对划分后的小数组（较主元小的值组成的子数组，以及较主元大的值组成的
+// 子数组）重复之前的两个步骤，直至数组已完全排序。
+function quickSort(array) {
+  return quick(array, 0, array.length - 1)
+}
+function quick(array, left, right) {
+  let index
+  if (array.length > 1) {
+    index = partition(array, left, right)
+    if (left < index - 1) {
+      quick(array, left, index - 1)
+    }
+    if (index < right) {
+      quick(array, index, right )
+    }
+  }
+  return array
+}
+function partition(array, left, right) {
+  const pivot = array[Math.floor((right + left) / 2)]
+  let i = left
+  let j = right
+  while (i <= j) {
+    while (array[i] < pivot) {
+      i++
+    }
+    while (array[j] > pivot) {
+      j--
+    }
+    if (i <= j) {
+      [array[i], array[j]] = [array[j], array[i]]
+      // swap(array, i, j)
+      i++
+      j--
+    }
+  }
+  return i
+}
+
 export {
   bubbleSort,
   selectionSort,
   insertionSort,
-  mergeSort
+  mergeSort,
+  quickSort
 }
