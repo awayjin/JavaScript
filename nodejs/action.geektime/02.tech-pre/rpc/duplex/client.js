fc61f87b25800380c1e16134dc3f61b3bfcccc90
金伟 <jinwei@jinweideMacBook-Pro.local>
2019-11-26 08:02:13 +0800

    feat: duplex

--- nodejs/action.geektime/02.tech-pre/rpc/duplex/client.js
+++ nodejs/action.geektime/02.tech-pre/rpc/duplex/client.js
@@ -6,10 +6,57 @@ let client = net.createConnection(
   },
   () => {
     console.log('1. 已连接到服务器')
-    client.write('发送数据， 我来自 Client')
+    // client.write('发送数据， 我来自 Client')
   }
 )
 
+const LESSON_IDS = [
+  "136797",
+  "136798",
+  "136799",
+  "136800",
+  "136801",
+  "136803",
+  "136804",
+  "136806",
+  "136807",
+  "136808",
+  "136809",
+  "141994",
+  "143517",
+  "143557",
+  "143564",
+  "143644",
+  "146470",
+  "146569",
+  "146582"
+]
+
+
+let id = ''
+let seq = 0
+function writeLessonId () {
+  let index = Math.floor(Math.random() * LESSON_IDS.length)
+  id = LESSON_IDS[index]
+
+  let buffer = Buffer.alloc(6)
+  buffer.writeInt16BE(seq)
+  buffer.writeInt32BE(id)
+  client.write(buffer)
+  // client.write(id)
+  seq++
+}
+
+let k = 0
+while (k  < 100)  {
+  writeLessonId()
+  console.log(`${k} ${id}`)
+  k++
+}
+
 client.on('data', (buffer) => {
-  console.log('2. Client 接收数据 str: ', buffer.toString(), ', buffer:', buffer)
+  console.log('\n2. Client 接收数据 str: ', buffer.toString(), ', buffer:', buffer)
+  writeLessonId()
 })
+
+
