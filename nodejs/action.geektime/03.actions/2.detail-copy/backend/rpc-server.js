f5c3b844e2660df44db2f420707e583544d20053
金伟 <jinwei@jinweideMacBook-Pro.local>
2019-12-15 22:26:07 +0800

    fea

--- nodejs/action.geektime/03.actions/2.detail-copy/backend/rpc-server.js
+++ nodejs/action.geektime/03.actions/2.detail-copy/backend/rpc-server.js
@@ -1,4 +1,7 @@
 const net = require('net')
+const fs = require('fs')
+const protoBuffers = require('protocol-buffers')
+const messages = protoBuffers(fs.readFileSync('../rpc/detail.proto'))
 
 class RPC {
   constructor ({ encodeResponse, decodeRequest, isCompleteRequest }) {
@@ -9,3 +12,19 @@ class RPC {
 
   }
 }
+
+const server = net.createServer((socket) => {
+  socket.on('data', (buffer) => {
+    console.log('buffer:', buffer)
+    console.log('buffer.readInt32BE:', buffer.readInt32BE())
+    console.log('buffer.readInt32BE(4):', buffer.readInt32BE(4))
+    console.log('buffer.slice(8):', buffer.slice(8))
+    const columnId = messages.ColumnRequest.decode(buffer.slice(8))
+    console.log('messages.ColumnRequest.decode(buffer.slice(8)):', columnId)
+
+  })
+
+
+})
+
+server.listen(4000)
