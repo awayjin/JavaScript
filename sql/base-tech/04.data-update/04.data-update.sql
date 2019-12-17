cfa61a2a32e7b082ff80286c99a08c5b4cb7531f
awayjin <258246377@qq.com>
2019-12-17 16:57:32 +0800

    feat: scp and sql

--- sql/base-tech/04.data-update/04.data-update.sql
+++ sql/base-tech/04.data-update/04.data-update.sql
@@ -15,6 +15,8 @@ VALUES
 ('0001', 'T恤衫', '衣服', 1000, 500, '2009-09-20');
 -- INSERT
 INSERT INTO ProductIns VALUES('0006', '圆珠笔', '办公用品', 3, 1.2, '2019-12-09');
+INSERT INTO ProductIns VALUES('07-2', '圆珠笔', '办公用品', 3, null, '2019-12-09');
+SELECT * FROM ProductIns;
 
 -- 多行INSERT （Oracle以外）
 INSERT INTO ProductIns VALUES 
@@ -22,5 +24,19 @@ INSERT INTO ProductIns VALUES
 ('0003', '运动T恤', '衣服', 4000, 2800, NULL),
 ('0004', '菜刀', '厨房用具', 3000, 2800, '2009-09-20');
 
+SELECT * FROM productins;
 
-SELECT * FROM ProductIns;
+-- DEFAULT 约束, 创建表时来设定默认值
+DROP TABLE DefaultProduct;
+CREATE TABLE DefaultProduct(
+	product_id CHAR(4) NOT NULL,
+	sale_price INTEGER DEFAULT 0, -- 销售单值的默认值设为 0
+	PRIMARY KEY (product_id)
+);
+INSERT INTO DefaultProduct VALUES(1, DEFAULT);
+SELECT * FROM DefaultProduct;
+
+
+
+
+
