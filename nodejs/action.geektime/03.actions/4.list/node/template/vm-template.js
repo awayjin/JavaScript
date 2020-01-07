1060766372ee0fabcf45f1aaf48a7d98b0f2e105
awayjin <258246377@qq.com>
2020-01-07 17:32:26 +0800

    feat: vm

--- nodejs/action.geektime/03.actions/4.list/node/template/vm-template.js
+++ nodejs/action.geektime/03.actions/4.list/node/template/vm-template.js
@@ -10,5 +10,11 @@ const sandbox = {
 }
 vm.runInNewContext('count += 1; name = "kitty"', sandbox)
 
-console.log('sandbox:', sandbox)
-console.log(util.inspect(sandbox))
+console.log('sandbox:', sandbox) // { animal: 'cat', count: 3, name: 'kitty' }
+// console.log(util.inspect(sandbox))
+
+const two = vm.runInNewContext('count = 1;c=11;name3 = "hello"')
+console.log('two:', two)
+
+const three = vm.runInNewContext('{ a: 331, b=1222222222222222224}')
+console.log('three:', three)
