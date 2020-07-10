9c653e250958208d686ce2dd69eea91a5041cca3
awayjin <258246377@qq.com>
2020-07-10 15:50:45 +0800

    feat: effect clean

--- react/ts-demo/src/pages/useEffect/index.tsx
+++ react/ts-demo/src/pages/useEffect/index.tsx
@@ -10,13 +10,29 @@ interface IState {
 }
 
 export function UseEffectPage () {
+  // const []
   const [count, setCount] = useState(30);
   useEffect(() => {
     document.title = `${count}.You clicked ${count} times`
   })
+  const [seconds, setSeconds] = useState(0);
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setSeconds(seconds + 1)
+      console.log('seconds:', seconds)
+    }, 1000)
+    // 需要清除的 effect, 定时器
+    // class 方式 https://blog.csdn.net/csugonghang/article/details/83834116
+    return () => {
+      clearInterval(timer)
+    }
+  }, [seconds])
   return (
     <>
       <h2>You clicked {count} times.</h2>
+      <h3>定时器：
+        { seconds }
+      </h3>
       <button onClick={ () => { setCount(count + 1)}}>
         Click me
       </button>
---
+++ react/ts-demo/src/pages/useEffect/useEffect.md
@@ -0,0 +1,3 @@
+https://juejin.im/post/5d985deae51d4577f9285c2f
+
+## 3333
