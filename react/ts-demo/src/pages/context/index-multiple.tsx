b676418795801171d63f94f04f49a2099d4474bb
awayjin <258246377@qq.com>
2020-08-13 14:56:49 +0800

    feat: diff

--- react/ts-demo/src/pages/context/index-multiple.tsx
+++ react/ts-demo/src/pages/context/index-multiple.tsx
@@ -13,8 +13,8 @@ export class ContextPage extends React.Component<any, any> {
   }
   render() {
     // const { theme, singedInUser } = this.props
-    // const { theme, singedInUser } = { theme: 'dd', singedInUser: { name: 'guest3 '}}
-    const { theme, singedInUser } = { theme: 'dd', singedInUser: 'guest2' }
+    const { theme, singedInUser } = { theme: 'dd', singedInUser: { name: 'guest3 '}}
+    // const { theme, singedInUser } = { theme: 'dd', singedInUser: 'guest2' }
     return (
       <>
         <h2>multiple content</h2>
@@ -25,8 +25,9 @@ export class ContextPage extends React.Component<any, any> {
               <UserContext.Consumer>
                 {user => (
                   <div>
-                    { user}
-                   ---theme: { theme }
+                    { user.name } 
+                     { theme }
+                   ---theme:
                   </div>
                 )}
               </UserContext.Consumer>
--- react/ts-demo/src/pages/reducer/index.tsx
+++ react/ts-demo/src/pages/reducer/index.tsx
@@ -1,40 +1,27 @@
 import React, {useReducer} from 'react';
 // 简介： https://juejin.im/post/5c87c1b4f265da2dc453b6d6
-import Buttons from './Buttons';
-import ShowArea from './ShowArea'
-import { Color } from './Color';   // 引入Color组件
-import {TreeContent} from "./treeContent";
-import {ServiceLine} from "./components/serviceLine";
-import {StatInfo} from "./components/SateInfo";
 
-// export function ReducerPage () {
-//   const [count, dispatch] = useReducer((state: any, action: any) => {
-//     switch (action) {
-//       case 'add':
-//         return state + 1;
-//       case 'sub':
-//         return state - 1;
-//     }
-//   }, 0);
-//   return (
-//     <div>
-//       <h2>count: { count }</h2>
-//       <button onClick={() => dispatch('add')}>add</button>
-//       <button onClick={() => dispatch('sub')}>sub</button>
-//     </div>
-//   );
-// }
 
 export const ReducerPage = () => {
+  const initState = { count: 0 }
+  const reducer = (state: any, action: any) => {
+    switch (action.type) {
+      case 'increment':
+        return { count: state.count + 1}
+      case 'decrement':
+        return { count: state.count - 1}
+      default:
+        throw new Error('no type')
+    }
+  }
+  const [state, dispatch] = useReducer(reducer, initState)
   return (
     <>
-      <Color>
-        <Buttons />
-        <ShowArea />
-      </Color>
-      <ServiceLine>
-        <TreeContent><></></TreeContent>
-      </ServiceLine>
+      <div>ReducerPage. count: { state.count }</div>
+      <p>
+        <button onClick={ () => { dispatch({ type: 'increment'}) }}>add</button>
+        <button onClick={ () => { dispatch({ type: 'decrement'}) }}>sub</button>
+      </p>
     </>
   )
 }
