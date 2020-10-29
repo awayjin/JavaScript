221675114a099b596eab3f04c5d648259c954681
金伟 <jinwei@jinweideMacBook-Pro.local>
2020-10-30 07:58:14 +0800

    feat: useCallback

---
+++ react/my-antd/umi-demo/src/pages/function/hook/index.tsx
@@ -0,0 +1,55 @@
+import React, {useCallback, useEffect, useMemo, useState} from "react";
+import { connect } from 'dva'
+
+function Dva (props: any) {
+  const [count, setCount] = useState(10);
+
+
+  const [text, setText] = useState('text-demo');
+
+  const noCacheText = ()=> {
+    console.log('text changed')
+    return text
+  }
+  const memoText = useMemo(() => {
+    console.log('memoText')
+    return text
+  }, [text]);
+
+  async function demo() {
+    console.log('demo')
+    fetch('/api/getList?id=3')
+  }
+  useEffect(() => {
+    // fetch('/api/getList?id=3')
+    demo();
+  })
+  // console.log('props:', props)
+  // demo();
+  // console.log(22)
+
+  const handleCountCB = useCallback(() => {
+    console.log('log changed useCallback')
+    setCount(count+ 1)
+  }, [count])
+  const handleCount = ()=> {
+    setCount(count + 1)
+  }
+  return <>
+    <h2>hook</h2>
+    <p onClick={handleCountCB}>
+      handleCountCB: { count }
+    </p>
+    <p onClick={handleCount}>
+      onClick: { count }
+    </p>
+    {/*<p>{ noCacheText() }</p>*/}
+    <p>{ memoText }</p>
+  </>
+}
+
+// @ts-ignore
+export default connect(({ search }) => ({
+  search
+}))(Dva);
+
