d65d8444d8195f5fa0228c9788dd85c950d653b6
金伟 <jinwei@jinweideMacBook-Pro.local>
2020-11-25 07:56:11 +0800

    feat: thread

--- nodejs/trip-egg-react/node-demo/process/master.js
+++ nodejs/trip-egg-react/node-demo/process/master.js
@@ -1,6 +1,8 @@
 const { fork } = require('child_process')
 const child = fork('./child.js')
 
+console.log(`process.pid ${process.pid}`)
+
 child.on('message', msg => {
   console.log(`master on 来自子进程：${msg}`)
 })
