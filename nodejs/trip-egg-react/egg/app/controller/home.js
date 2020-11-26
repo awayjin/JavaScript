f9f5a04e7c812b95834fea46627e12f8cf01f764
金伟 <jinwei@jinweideMacBook-Pro.local>
2020-11-27 07:58:13 +0800

    feat: egg plugin

--- nodejs/trip-egg-react/egg/app/controller/home.js
+++ nodejs/trip-egg-react/egg/app/controller/home.js
@@ -1,15 +1,22 @@
 'use strict';
 
 const Controller = require('egg').Controller;
+// const info = require('../../utils/info');
 
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
+    // console.log(info());
+    console.log(ctx.request.url);
+    console.log(ctx.info);
     ctx.body = 'hi, egg app fi';
   }
 
   async demo() {
     const { ctx } = this;
+    // console.log(info());
+    console.log(ctx.request.url);
+    console.log(ctx.info);
     ctx.body = 'demo page';
   }
 }
